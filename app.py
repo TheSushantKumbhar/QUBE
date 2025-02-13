a71baf6ab883a5b557e5736c3238dead22f96af6
Sushant Kumbhar <me.sushantkumbhar@gmail.com>
2025-02-13 21:00:49 +0530

    home page frontend

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ app.register_blueprint(auth_bp, url_prefix='/auth')
 
 @app.route('/')
 def home():
-    return render_template('login.html')
+    return render_template('index.html')
+
 
 @app.route('/dashboard')
 def dashboard():
