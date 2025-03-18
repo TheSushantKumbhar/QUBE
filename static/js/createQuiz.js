eef8b66b118626d7c64d4646da2968143a4224de
Sushant Kumbhar <me.sushantkumbhar@gmail.com>
2025-03-19 01:24:15 +0530

    createQuiz added

--- static/js/createQuiz.js
+++ static/js/createQuiz.js
@@ -1,3 +1,4 @@
+
 document.getElementById("startQuiz").addEventListener("click", function() {
     document.getElementById("page1").classList.add("hidden");
     document.getElementById("page2").classList.remove("hidden");
