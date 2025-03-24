958d90b1e64c89eb935b1755ac76297eec9612e6
Sushant Kumbhar <me.sushantkumbhar@gmail.com>
2025-03-24 23:31:12 +0530

    changes in takeQUIZ

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: ["./templates/**/*.html", "./static/css/**/*.css"],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./templates/**/*.html", "./static/css/**/*.css"],
+  darkMode: 'class', // Enables dark mode using the 'class' strategy
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        pulse: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0.7 },
+        }
+      },
+      animation: {
+        pulse: 'pulse 1s infinite',
+      }
+    }
   },
   plugins: [],
-}
-
+};
